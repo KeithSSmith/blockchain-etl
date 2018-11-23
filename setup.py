593de639788a502daa88a7e2eadb4c8c1d14896e
Keith Smith <keith.scotts@gmail.com>
2018-11-23 14:00:28 -0600

    Adding TMN token

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open("README.rst", "r") as fh:
 setup(
     name='blockchain-etl',
     python_requires='>=3.6',
-    version='0.1.0',
+    version='0.1.1',
     author='Keith Smith',
     author_email='keith.scotts@gmail.com',
     license='MIT License',
